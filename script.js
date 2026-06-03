49c4be6dd2eb2f0713c4563e626738f43e402a89
ramlaxmansita369-blip <ramlaxmansita369@gmail.com>
2026-06-03 14:21:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,136 +1,18 @@
-let employees =
-JSON.parse(localStorage.getItem("employees")) || [];
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-function registerEmployee(){
-
-let name =
-document.getElementById("name").value;
-
-let email =
-document.getElementById("email").value;
-
-let birthday =
-document.getElementById("birthday").value;
-
-let employee = {
-name,
-email,
-birthday
+const firebaseConfig = {
+  apiKey: "AIzaSyCBjaghgiluUgkElOg0fQwg0IfDMU0BcNU",
+  authDomain: "employee-details-1ce02.firebaseapp.com",
+  projectId: "employee-details-1ce02",
+  storageBucket: "employee-details-1ce02.firebasestorage.app",
+  messagingSenderId: "656589537477",
+  appId: "1:656589537477:web:d3ffafa4061b7fb869b0ac",
+  measurementId: "G-JSXRRMCXN0"
 };
 
-employees.push(employee);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-localStorage.setItem(
-"employees",
-JSON.stringify(employees)
-);
-
-alert("Employee Registered");
-
-loadBirthdays();
-}
-
-function loginAttendance(){
-
-let loginTime =
-new Date().toLocaleString();
-
-localStorage.setItem(
-"loginTime",
-loginTime
-);
-
-document.getElementById(
-"attendanceStatus"
-).innerHTML =
-"Logged In: " + loginTime;
-}
-
-function logoutAttendance(){
-
-let logoutTime =
-new Date().toLocaleString();
-
-localStorage.setItem(
-"logoutTime",
-logoutTime
-);
-
-document.getElementById(
-"attendanceStatus"
-).innerHTML =
-"Logged Out: " + logoutTime;
-}
-
-function applyLeave(){
-
-let date =
-document.getElementById("leaveDate").value;
-
-let reason =
-document.getElementById("leaveReason").value;
-
-let leaves =
-JSON.parse(
-localStorage.getItem("leaves")
-) || [];
-
-leaves.push({
-date,
-reason
-});
-
-localStorage.setItem(
-"leaves",
-JSON.stringify(leaves)
-);
-
-alert("Leave Applied");
-}
-
-function submitReport(){
-
-let report =
-document.getElementById("workReport").value;
-
-let reports =
-JSON.parse(
-localStorage.getItem("reports")
-) || [];
-
-reports.push({
-date:new Date().toLocaleDateString(),
-report:report
-});
-
-localStorage.setItem(
-"reports",
-JSON.stringify(reports)
-);
-
-alert("Work Report Submitted");
-}
-
-function loadBirthdays(){
-
-let list =
-document.getElementById("birthdayList");
-
-list.innerHTML = "";
-
-employees.forEach(emp=>{
-
-let li =
-document.createElement("li");
-
-li.innerHTML =
-emp.name +
-" - " +
-emp.birthday;
-
-list.appendChild(li);
-
-});
-}
-
-loadBirthdays();
+// Initialize Firestore and export it for use in other files
+export const db = getFirestore(app);
